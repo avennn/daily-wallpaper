6d459e124a9c77c57364467f39a072303c57ea40
liangjianwen <liangjianwen@kuaishou.com>
2021-04-22 00:41:13 +0800

    fix: plist PATH and process.env._ problem

--- src/plist.ts
+++ src/plist.ts
@@ -7,20 +7,39 @@ import logger from './logger';
 
 export function generatePListText(auto: boolean = true) {
     // https://medium.com/@chetcorcos/a-simple-launchd-tutorial-9fecfcf2dbb3
+    let cmdPath = process.env._;
+    logger.log('\nprocess.env._: ', process.env._);
+    if (cmdPath && cmdPath.endsWith('dwp')) {
+        cmdPath = cmdPath.replace(/^(.+)(dwp)$/, '$1node');
+    }
     const config = {
         Label: plistLabel,
         RunAtLoad: auto,
         StandardErrorPath: stdErrorLog,
         StandardOutPath: stdOutLog,
         EnvironmentVariables: {
-            PATH: `<![CDATA[${process.env.PATH}]]>`,
+            // TODO: 需要cdata包装吗
+            // PATH: `<![CDATA[${process.env.PATH}]]>`,
+            PATH: process.env.PATH!,
         },
-        ProgramArguments: [process.env._!, 'start'],
+        ProgramArguments: [
+            // can't directly use 'dwp', cause permission error
+            cmdPath!,
+            // TODO: 有没有更好的写法
+            path.resolve(__dirname, '../index.js'),
+            'start',
+        ],
     };
+    logger.log('\npath: ', process.env.PATH);
+    logger.log('\ncmdPath: ', cmdPath);
     return plist.build(config);
 }
 
-export async function savePList(auto: boolean = true) {
+export function savePList(auto: boolean = true) {
+    if (!process.env._) {
+        // not start with node or dwp
+        return;
+    }
     createFileIfNotExist(stdErrorLog);
     createFileIfNotExist(stdOutLog);
     try {
@@ -31,6 +50,6 @@ export async function savePList(auto: boolean = true) {
     }
 }
 
-export async function setAutoStartup(auto: boolean = true) {
-    return await savePList(auto);
+export function setAutoStartup(auto: boolean = true) {
+    return savePList(auto);
 }
