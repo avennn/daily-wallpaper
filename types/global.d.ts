e1d59cbf8f4c895febc3145e7c80a24506a53823
javenleung <914301050@qq.com>
2021-11-23 00:06:52 +0800

    docs: optimize package.json

--- types/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'topparser' {
-    export function start();
-    export function on(eventName: string, fn: (...args: any[]) => void);
-    export function stop();
-}
-declare module 'table-parser' {
-    export function parse(data: any);
-}
