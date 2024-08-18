a05913e22cc1b4603a90d5fabefd06cfaf320cb0
javenleung <914301050@qq.com>
2024-08-18 17:26:52 +0800

    feat: remove safeJsonParse

--- src/utils/getAppVersion.ts
+++ src/utils/getAppVersion.ts
@@ -1,12 +1,15 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import dirname from '@/system/dirname';
-import { safeJsonParse } from 'tori';
 
 export default async function getAppVersion() {
 	const pkgJsonText = await fs.readFile(path.join(dirname(import.meta.url), '../../package.json'), {
 		encoding: 'utf8',
 	});
-	const { version } = safeJsonParse(pkgJsonText, { version: '' });
-	return version;
+	try {
+		const { version } = JSON.parse(pkgJsonText);
+		return version;
+	} catch (e) {
+		return '';
+	}
 }
